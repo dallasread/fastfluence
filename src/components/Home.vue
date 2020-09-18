0fc331f7d81a5908574ece8eff83cb2136566f7a
Dallas Read <dallas@excitecreative.ca>
2020-09-17 21:56:19 -0300

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,7 +3,7 @@
     <h1>Welcome to Fastfluence.</h1>
     <p v-if="app.pages.length"><strong>Your Fastfluence is ready!</strong></p>
     <p>Get started by searching for something in the sidebar.</p>
-    <p>Pages are stored in this browser's local storage as you view them.</p>
+    <p>As you view each page, they are stored in this browser's local storage (and kept up to date).</p>
     <p>As a bonus, you can <a href="javascript:;" v-on:click="app.updateAllPages()">Download All Content</a> now.</p>
   </div>
 </template>
