8c8bd150c0575df42cdce4baeb5741ddee619b2a
Dallas Read <dallas@excitecreative.ca>
2022-10-13 08:17:05 -0300

    Update deploy script

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -1,26 +1,46 @@
 /* eslint-disable no-console */
+
 const execa = require('execa')
-const fs = require('fs');
-(async () => {
-  try {
-    await execa('git', ['checkout', '--orphan', 'gh-pages'])
-    // eslint-disable-next-line no-console
-    console.log('Building started...')
-    await execa('npm', ['run', 'build'])
-    // Understand if it's dist or build folder
-    const folderName = fs.existsSync('dist') ? 'dist' : 'build'
-    await execa('git', ['--work-tree', folderName, 'add', '--all'])
-    await execa('git', ['--work-tree', folderName, 'commit', '-m', 'gh-pages', '--no-verify'])
-    console.log('Pushing to gh-pages...')
-    await execa('git', ['push', 'origin', 'HEAD:gh-pages', '--force'])
-    await execa('rm', ['-r', folderName])
-    await execa('rm', ['-rf', '.git/gc.log'])
-    await execa('git', ['checkout', '-f', 'master'])
-    await execa('git', ['branch', '-D', 'gh-pages'])
-    console.log('Successfully deployed, check your settings')
-  } catch (e) {
-    // eslint-disable-next-line no-console
+const fs = require('fs')
+const deployBranch = 'gh-pages'
+const masterBranch = 'master'
+const target = 'dist'
+
+const refresh = async () => {
+  console.log('🌱 Creating fresh branch...')
+
+  await execa('git', ['checkout', '--orphan', deployBranch])
+}
+
+const build = async () => {
+  console.log('🔨 Building...')
+
+  await execa('yarn', ['build'])
+  await execa('cp', [`${target}/index.html`, `${target}/404.html`])
+  await execa('git', ['--work-tree', target, 'add', '--all'])
+  await execa('git', ['--work-tree', target, 'commit', '-m', deployBranch, '--no-verify'])
+}
+
+const push = async () => {
+  console.log('🌏 Deploying...')
+
+  await execa('git', ['push', 'origin', `HEAD:${deployBranch}`, '--force'])
+  await execa('rm', ['-r', target])
+  await execa('rm', ['-rf', '.git/gc.log'])
+  await execa('git', ['checkout', '-f', masterBranch])
+  await execa('git', ['branch', '-D', deployBranch])
+}
+
+const done = () => {
+  console.log('✅ Deployed!')
+}
+
+Promise.resolve()
+  .then(refresh)
+  .then(build)
+  .then(push)
+  .then(done)
+  .catch((e) => {
     console.log(e.message)
     process.exit(1)
-  }
-})()
+  })
