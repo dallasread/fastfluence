33aecc77fce0021a361f28467cb6ca61bfeca0d8
Dallas Read <dallas@excitecreative.ca>
2022-10-13 08:16:13 -0300

    Trigger deploy

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/fastfluence/' : '',
   pwa: {
     themeColor: '#1C25C1',
-    msTileColor: '#1C25C1'
+    msTileColor: '#1C25C1',
+    workboxOptions: {
+      exclude: ['CNAME'],
+      maximumFileSizeToCacheInBytes: 5000000
+    }
   }
 }
