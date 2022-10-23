4210d9e24fb4ce97a5966b0f2344dd1515346d5d
Dallas Read <dallas@excitecreative.ca>
2022-10-22 22:33:55 -0300

    Maybe faster loadign?

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,15 @@
 module.exports = {
   pwa: {
+    name: 'Fastfluence',
     themeColor: '#1C25C1',
     msTileColor: '#1C25C1',
+    appleMobileWebAppCapable: 'yes',
     workboxOptions: {
       exclude: ['CNAME'],
       maximumFileSizeToCacheInBytes: 5000000
     }
+  },
+  css: {
+    sourceMap: process.env.NODE_ENV === 'production'
   }
 }
